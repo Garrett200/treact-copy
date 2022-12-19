953c8583b21fc240abfea921c614eb1a1bd469e8
Garrett <garrettjrh@live.com>
2022-12-19 08:42:33 -0700

    Added animations for each section based on wether they're in view

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
-const numSteps = 20.0;
-
-let element;
-let prevRatio = 0.0;
-let increasingColor = "rgba(40, 40, 190, ratio)";
-let decreasingColor = "rgba(190, 40, 40, ratio)";
-
-
-window.addEventListener("load", (event) => {
-    element = document.querySelector("animation__enter-right");
-
-    createObserver();
-}, false);
-
-
-function createObserver() {
-    let observer;
-
-    let options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: buildThresholdList();
-    };
-
-    observer = new IntersectionObserver(handleIntersect, options);
-    observer.observe(element);
-}
-
-function buildThresholdList() {
-    let thresholds = [];
-    let numSteps = 20;
-  
-    for (let i=1.0; i<=numSteps; i++) {
-      let ratio = i/numSteps;
-      thresholds.push(ratio);
-    }
-  
-    thresholds.push(0);
-    return thresholds;
-  }
-  
-
-
-function handleIntersect(entries, observer) {
-    entries.forEach((entry) => {
-        if (entry.intersectionRatio > prevRatio) {
-            addClass(entries, "animation__enter-left")
-        } else {
-            entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
-        }
-
-        prevRatio = entry.intersectionRatio;
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("enter-left", entry.isIntersecting);
     });
-}
+};
 
+const inViewport2 = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("enter-right", entry.isIntersecting);
+    });
+};
 
+const Obs2 = new IntersectionObserver(inViewport2);
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {};
 
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('.animation__enter-left');
+ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+    });
 
-
+const ELs_inViewport2 = document.querySelectorAll('.animation__enter-right');
+ELs_inViewport2.forEach(EL => {
+    Obs2.observe(EL, obsOptions);
+});
 
 /*
 document.addEventListener("DOMContentLoaded", function(event) {
